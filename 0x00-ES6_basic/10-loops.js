4e32e56096d35944e8bc6a37040715ba247118b2
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-05 01:51:33 +0200

    append array function implemented.

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,9 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   const arrayCopy = [];
 
   for (const idx in array) {
-    if (idx < appendString.length) {
-      arrayCopy.push(`${appendString}${array[idx]}`);
-    }
+      arrayCopy.push(appendString + array[idx]);
   }
 
   return arrayCopy;
