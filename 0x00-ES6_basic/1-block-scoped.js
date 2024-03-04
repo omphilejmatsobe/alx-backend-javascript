109038cfef42130c7e8f1c2722aea89d93253744
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-04 19:29:02 +0200

    block-scoped.js functions updated.

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
 
-  if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
-  }
+  const task = false;
+  const task2 = true;
 
-  var task = false;
-  var task2 = true;
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
 
   return [task, task2];
 }
