4bf8a09a6e34021c94f352d56d8d0e6ddf35e605
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-05-04 00:51:59 +0200

    calcul_chai test

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,24 @@
+function calculateNumber(type, a, b) {
+    const A = Math.round(a);
+    const B = Math.round(b);
+    let res = 0;
+
+    switch (type) {
+    case 'SUM':
+	res = A + B;
+	break;
+    case 'SUBTRACT':
+	res = A - B;
+	break;
+    case 'DIVIDE':
+      if (B === 0) {
+            res = "Error";
+      } else {
+            res = A / B;
+      }
+      break;
+    }
+    return res;
+}
+
+module.exports = calculateNumber;
