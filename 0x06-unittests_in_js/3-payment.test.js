62ff26449d0415c18ab967b8df18ca08700155ee
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-05-04 01:06:56 +0200

    payment class update

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,16 @@
-const {describe, it} = require("mocha");
-const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./3-payment");
-const Utils = require("./utils");
-const assert = require("assert");
+const { describe, it } = require('mocha');
+const sinon = require('sinon');
+const assert = require('assert');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
 
-describe("sendPaymentRequestToApi", function() {
-    it("checks for Utils.calculateNumber call", function() {
-	const spy = sinon.spy(Utils, "calculateNumber");
+describe('sendPaymentRequestToApi', () => {
+  it('checks for Utils.calculateNumber call', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
 
-	sendPaymentRequestToApi(50, 24.52);
+    sendPaymentRequestToApi(50, 24.52);
 
-	assert(spy.calledOnce);
-	spy.restore();
-    });
+    assert(spy.calledOnce);
+    spy.restore();
+  });
 });
